324925f20c789d57c0dba18486e1d337359177b6
kmdnc <41956765+kmdnc@users.noreply.github.com>
2020-11-04 01:08:19 +0500

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,16 +20,16 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'wordpress' );
 
 /** Имя пользователя MySQL */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'wordpress' );
 
 /** Пароль к базе данных MySQL */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'wordpress-pass' );
 
 /** Имя сервера MySQL */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'wordpress.cbs8omqsohea.eu-west-3.rds.amazonaws.com' );
 
 /** Кодировка базы данных для создания таблиц. */
 define( 'DB_CHARSET', 'utf8' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'впишите сюда уникальную фразу' );
-define( 'SECURE_AUTH_KEY',  'впишите сюда уникальную фразу' );
-define( 'LOGGED_IN_KEY',    'впишите сюда уникальную фразу' );
-define( 'NONCE_KEY',        'впишите сюда уникальную фразу' );
-define( 'AUTH_SALT',        'впишите сюда уникальную фразу' );
-define( 'SECURE_AUTH_SALT', 'впишите сюда уникальную фразу' );
-define( 'LOGGED_IN_SALT',   'впишите сюда уникальную фразу' );
-define( 'NONCE_SALT',       'впишите сюда уникальную фразу' );
+define('AUTH_KEY',         'j3*x](INQl.cIh_%+.;gA ;+GvW7Klc[NwkhaxfOG2`aOC}8+,~T>(2aTjz7]-4j');
+define('SECURE_AUTH_KEY',  'smY*B0k<e%#Hhyigp{IVH^N~JfFJ@|qcvB*3OR_~[8#74<L{37*<p?MeV?|H=Bh2');
+define('LOGGED_IN_KEY',    'HO/}Ex49[=m:jC-]zWEC(tv1[f^0sAH4J])nuX`^;,ZZNEh;S]N9kg9/(pVMLO=|');
+define('NONCE_KEY',        'K/y,8~,sZ)Q: x54]k5vH6w 6]E+@D3j,(Jq2i1vZ3ps.Q+&/;1s8lpTH*oTRj0|');
+define('AUTH_SALT',        '?/2Z$ftk:bn,5SXjmO7OCQv3Om}N0]JRX=<,%~!gB-zeq/##_5k{<M2qM<2c.d`X');
+define('SECURE_AUTH_SALT', '&,OKB~A]IWZYH-vIy}1LXUZF`dDQj-2E)|O=pBLlXszKDDSlGedbD=2m=0A hy)C');
+define('LOGGED_IN_SALT',   'Y9%=4a,<XUijeLaL-^w^Vg#Rn 4GOl(a#sz3jER/w=WiO|5Zprl07.kWK-46=L61');
+define('NONCE_SALT',       '{Dro66z&[jS| SlTyY{# Z7xYL[A^|Z?6Xgvy-V.=;ej3-705:aKA PiiwousJ=j');
 
 /**#@-*/
 
